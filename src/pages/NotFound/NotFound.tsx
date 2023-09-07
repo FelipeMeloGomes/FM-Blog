764541abf12bbf6b980e674f5e6f59f5e62ab6cb
Felipe Melo <felipemelogomes36@gmail.com>
2023-09-07 01:23:27 -0300

    feat: add imagem notfound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,10 +4,16 @@ import { memo } from "react";
 // estilos
 import styles from "./NotFound.module.css";
 
-
+// imagem
+import notfound from "./notfound.jpg";
 
 const NotFound = () => {
-    return <h1 className={styles.h1}>Ooops... Essa página não existe.</h1>;
+    return (
+        <div>
+            <h1 className={styles.h1}>Ooops... Essa página não existe.</h1>;
+            <img src={notfound} alt="erro 404 imagem" />
+        </div>
+    );
 };
 
 export default memo(NotFound);
