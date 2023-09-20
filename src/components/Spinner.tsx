9e7d15342ec1c632605691b58f0cd818c56ebe7a
Felipe Melo <felipemelogomes36@gmail.com>
2023-09-20 12:28:05 -0300

    feat: responsividade Spinner finalizado

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,11 @@
 import styles from "./Spinner.module.css";
 
 const Spinner = () => {
-    return <div className={styles.loader}></div>;
+    return (
+        <div className={styles.loader_container}>
+            <div className={styles.loader}></div>
+        </div>
+    );
 };
 
 export default Spinner;
