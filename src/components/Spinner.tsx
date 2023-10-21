e16b83f64073d9a2d8436d3cd8e70ffb5d8a6b1d
Felipe Melo <felipemelogomes36@gmail.com>
2023-10-21 13:28:01 -0300

    feat: novo spinner adicionado

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,40 @@
+import React from "react";
 // css
 import styles from "./Spinner.module.css";
 
 const Spinner = () => {
     return (
-        <div className={styles.loader_container}>
-            <div className={styles.loader}></div>
+        <div className={styles.loader}>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 66 66"
+                height="100px"
+                width="100px"
+                className={styles.spinner}
+            >
+                <circle
+                    stroke="url(#gradient)"
+                    r="20"
+                    cy="33"
+                    cx="33"
+                    stroke-width="1"
+                    fill="transparent"
+                    className={styles.path}
+                ></circle>
+                <linearGradient id="gradient">
+                    <stop stop-opacity="1" stop-color="#000" offset="0%"></stop>
+                    <stop
+                        stop-opacity="1"
+                        stop-color="#f6fcff"
+                        offset="50%"
+                    ></stop>
+                    <stop
+                        stop-opacity="0"
+                        stop-color="#f6fcff"
+                        offset="100%"
+                    ></stop>
+                </linearGradient>
+            </svg>
         </div>
     );
 };
