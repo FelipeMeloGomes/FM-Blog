229765625cd2bc19a0a93bb9c0c5a33a48f84cf9
Felipe Melo <felipemelogomes36@gmail.com>
2023-10-25 00:24:26 -0300

    style: novo botao de like

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,31 +1,31 @@
 // CSS
+
 import styles from "./LikeButton.module.css";
 
+// React
+import { useState } from "react";
+
 function LikeButton() {
+    const [likeCount, setLikeCount] = useState(0);
+
+    const handleLikeClick = () => {
+        setLikeCount(likeCount + 1);
+    };
     return (
-        <label className={styles.container}>
-            <input type="checkbox" />
-            <div className={styles.checkmark}>
+        <button className={styles.Btn} onClick={handleLikeClick}>
+            <span className={styles.leftContainer}>
                 <svg
-                    viewBox="0 0 50 50"
-                    version="1.1"
+                    fill="white"
+                    viewBox="0 0 512 512"
+                    height="1em"
                     xmlns="http://www.w3.org/2000/svg"
-                    className={styles.icon}
                 >
-                    <path
-                        d="M 24.10 6.29 Q 28.34 7.56 28.00 12.00 Q 27.56 15.10 27.13 18.19 A 0.45 0.45 4.5 0 0 27.57 18.70 Q 33.16 18.79 38.75 18.75 Q 42.13 18.97 43.23 21.45 Q 43.91 22.98 43.27 26.05 Q 40.33 40.08 40.19 40.44 Q 38.85 43.75 35.50 43.75 Q 21.75 43.75 7.29 43.75 A 1.03 1.02 0.0 0 1 6.26 42.73 L 6.42 19.43 A 0.54 0.51 -89.4 0 1 6.93 18.90 L 14.74 18.79 A 2.52 2.31 11.6 0 0 16.91 17.49 L 22.04 7.17 A 1.74 1.73 21.6 0 1 24.10 6.29 Z M 21.92 14.42 Q 20.76 16.58 19.74 18.79 Q 18.74 20.93 18.72 23.43 Q 18.65 31.75 18.92 40.06 A 0.52 0.52 88.9 0 0 19.44 40.56 L 35.51 40.50 A 1.87 1.83 5.9 0 0 37.33 39.05 L 40.51 23.94 Q 40.92 22.03 38.96 21.97 L 23.95 21.57 A 0.49 0.47 2.8 0 1 23.47 21.06 Q 23.76 17.64 25.00 12.00 Q 25.58 9.36 24.28 10.12 Q 23.80 10.40 23.50 11.09 Q 22.79 12.80 21.92 14.42 Z M 15.57 22.41 A 0.62 0.62 0 0 0 14.95 21.79 L 10.01 21.79 A 0.62 0.62 0 0 0 9.39 22.41 L 9.39 40.07 A 0.62 0.62 0 0 0 10.01 40.69 L 14.95 40.69 A 0.62 0.62 0 0 0 15.57 40.07 L 15.57 22.41 Z"
-                        fill-opacity="1.000"
-                    ></path>
-                    <circle
-                        r="1.51"
-                        cy="37.50"
-                        cx="12.49"
-                        fill-opacity="1.000"
-                    ></circle>
+                    <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"></path>
                 </svg>
-            </div>
-            
-        </label>
+                <span className={styles.like}>Like</span>
+            </span>
+            <span className={styles.likeCount}>{likeCount}</span>
+        </button>
     );
 }
 
