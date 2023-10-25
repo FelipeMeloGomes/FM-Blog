8bde7082f550095de354d2d69820c19dc4d60186
Felipe Melo <felipemelogomes36@gmail.com>
2023-10-24 23:03:22 -0300

    feat: semantica melhorada

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -10,15 +10,19 @@ import LikeButton from "./LikeButton";
 const PostDetail = ({ post }) => {
     return (
         <div className={styles.post_detail}>
-            <img src={post.image} alt={post.title} />
+            <div>
+                <img src={post.image} alt={post.title} />
+            </div>
             <h2>{post.title}</h2>
             <p className={styles.createdby}>{post.createdBy}</p>
             <div className={styles.tags}>
                 {post.tagsArray.map((tag) => (
-                    <p key={tag}>
-                        <span>#</span>
-                        {tag}
-                    </p>
+                    <div>
+                        <p key={tag}>
+                            <span>#</span>
+                            {tag}
+                        </p>
+                    </div>
                 ))}
             </div>
             <div className={styles.container_btn}>
