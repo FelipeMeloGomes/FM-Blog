47bfd89909fab213c3b874d1d08dd54e4383b151
Felipe Melo <felipemelogomes36@gmail.com>
2023-11-01 16:13:35 -0300

    feat: pasword toggle

---
+++ src/utils/passwordUtils.jsx
@@ -0,0 +1,18 @@
+// passwordUtils.js
+export const togglePasswordVisibility = (
+    inputRef,
+    passwordVisible,
+    setPasswordVisible
+) => {
+    const input = inputRef.current;
+
+    if (input) {
+        if (passwordVisible) {
+            input.type = "password";
+        } else {
+            input.type = "text";
+        }
+    }
+
+    setPasswordVisible(!passwordVisible);
+};
