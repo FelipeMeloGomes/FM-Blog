7c0ebb1e8fdc0b04bdf4c0e9a88b0e79904c7683
Felipe Melo <felipemelogomes36@gmail.com>
2023-11-07 11:01:11 -0300

    fix: removido css

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -4,36 +4,19 @@ import { Link } from "react-router-dom";
 // Estilos css
 import styles from "./PostDetail.module.css";
 
-// React
-import { useEffect, useState } from "react";
-
 // Components
 import LikeButton from "./LikeButton";
 
 const PostDetail = ({ post }) => {
-    const [imageLoaded, setImageLoaded] = useState(false);
-
-    useEffect(() => {
-        const img = new Image();
-        img.src = post.image;
-        img.onload = () => {
-            // A imagem foi carregada com sucesso
-            setImageLoaded(true);
-        };
-    }, [post.image]);
     return (
         <div className={styles.post_detail}>
             <figure>
-                {imageLoaded ? (
-                    <img
-                        width="500px"
-                        height="500px"
-                        src={post.image}
-                        alt={post.title}
-                    />
-                ) : (
-                    <div>Loading...</div>
-                )}
+                <img
+                    width="500px"
+                    height="500px"
+                    src={post.image}
+                    alt={post.title}
+                />
             </figure>
             <h2>{post.title}</h2>
             <p className={styles.createdby}>{post.createdBy}</p>
