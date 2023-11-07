4e5fcf617a639fc2817133d93a8b07ae738d104a
Felipe Melo <felipemelogomes36@gmail.com>
2023-11-07 10:56:48 -0300

    feat: add precarregar img

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -4,19 +4,36 @@ import { Link } from "react-router-dom";
 // Estilos css
 import styles from "./PostDetail.module.css";
 
+// React
+import { useEffect, useState } from "react";
+
 // Components
 import LikeButton from "./LikeButton";
 
 const PostDetail = ({ post }) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = post.image;
+        img.onload = () => {
+            // A imagem foi carregada com sucesso
+            setImageLoaded(true);
+        };
+    }, [post.image]);
     return (
         <div className={styles.post_detail}>
             <figure>
-                <img
-                    width="500px"
-                    height="500px"
-                    src={post.image}
-                    alt={post.title}
-                />
+                {imageLoaded ? (
+                    <img
+                        width="500px"
+                        height="500px"
+                        src={post.image}
+                        alt={post.title}
+                    />
+                ) : (
+                    <div>Loading...</div>
+                )}
             </figure>
             <h2>{post.title}</h2>
             <p className={styles.createdby}>{post.createdBy}</p>
