521ab7e3369fd2b21f3e177563a46def130a2bc7
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-23 00:11:56 -0300

    feat: Component Footer refactoring

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
     return (
-        <div className={styles.container__footer}>
+        <>
             <footer className={styles.footer}>
                 Desenvolvido por Felipe Melo &copy;
             </footer>
-        </div>
+        </>
     );
 };
 
