3276a44d393233fb4f8a7dd50e0c474399490c77
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-23 13:56:57 -0300

    feat: adjust Component TitleParagraph

--- src/components/TitleParagraph/TitleParagraph.jsx
+++ src/components/TitleParagraph/TitleParagraph.jsx
@@ -1,13 +1,43 @@
 // Estilos Css
 import styles from "./TitleParagraph.module.css";
 
-const TitleParagraph = ({ title = "", paragraph = "" }) => {
+import PropTypes from "prop-types";
+
+const TitleParagraph = ({
+    title = "",
+    paragraph = "",
+    padding = "",
+    marginBottom = "",
+    textAlign = "",
+    color = "",
+}) => {
     return (
         <>
-            <h1 className={styles.title}>{title}</h1>
-            <p className={styles.paragraph}>{paragraph}</p>
+            <h1 className={styles.title} style={{ padding, textAlign, color }}>
+                {title}
+            </h1>
+            <p
+                className={styles.paragraph}
+                style={{ marginBottom, textAlign, color }}
+            >
+                {paragraph}
+            </p>
         </>
     );
+};
+
+TitleParagraph.propTypes = {
+    padding: PropTypes.string,
+    marginBottom: PropTypes.string,
+    textAlign: PropTypes.string,
+    color: PropTypes.string,
+};
+
+TitleParagraph.defaultProps = {
+    padding: "",
+    marginBottom: "",
+    textAlign: "",
+    color: "",
 };
 
 export default TitleParagraph;
