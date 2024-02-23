240cfd4f41e6f1dc0f582c8c990206b202b8640f
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-23 11:04:48 -0300

    feat: adjust LayoutPage

--- src/components/LayoutPage/LayoutPage.jsx
+++ src/components/LayoutPage/LayoutPage.jsx
@@ -1,8 +1,34 @@
 // Estilos Css
 import styles from "./LayoutPage.module.css";
 
-const LayoutPage = ({ children }) => {
-    return <div className={styles.container}>{children}</div>;
+import PropTypes from "prop-types";
+
+const LayoutPage = ({
+    children,
+    height = "",
+    minHeight = "",
+    textAlign = "",
+}) => {
+    return (
+        <div
+            className={styles.container}
+            style={{ height, minHeight, textAlign }}
+        >
+            {children}
+        </div>
+    );
+};
+
+LayoutPage.propTypes = {
+    height: PropTypes.string,
+    minHeight: PropTypes.string,
+    textAlign: PropTypes.string,
+};
+
+LayoutPage.defaultProps = {
+    height: "100vh",
+    minHeight: "100vh",
+    textAlign: "center",
 };
 
 export default LayoutPage;
