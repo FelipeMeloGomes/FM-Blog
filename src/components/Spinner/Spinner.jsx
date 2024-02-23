9dc6d70e2b5fbc26aee98889c5f1526c97d50c19
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-23 11:05:28 -0300

    feat: adjust Spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,9 +1,16 @@
 // Estilos css
 import styles from "./Spinner.module.css";
 
-const Spinner = () => {
+import PropTypes from "prop-types";
+
+const Spinner = ({
+    startColor = "#000",
+    middleColor = "#f6fcff",
+    endColor = "#f6fcff",
+    height = "",
+}) => {
     return (
-        <div className={styles.loader}>
+        <div className={styles.loader} style={{ height }}>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 66 66"
@@ -12,7 +19,7 @@ const Spinner = () => {
                 className={styles.spinner}
             >
                 <circle
-                    stroke="url(#gradient)"
+                    stroke={`url(#gradient-${startColor}-${middleColor}-${endColor})`}
                     r="20"
                     cy="33"
                     cx="33"
@@ -20,22 +27,36 @@ const Spinner = () => {
                     fill="transparent"
                     className={styles.path}
                 ></circle>
-                <linearGradient id="gradient">
-                    <stop stopOpacity="1" stopColor="#000" offset="0%"></stop>
+                <linearGradient
+                    id={`gradient-${startColor}-${middleColor}-${endColor}`}
+                >
                     <stop
                         stopOpacity="1"
-                        stopColor="#f6fcff"
+                        stopColor={startColor}
+                        offset="0%"
+                    ></stop>
+                    <stop
+                        stopOpacity="1"
+                        stopColor={middleColor}
                         offset="50%"
                     ></stop>
                     <stop
                         stopOpacity="0"
-                        stopColor="#f6fcff"
+                        stopColor={endColor}
                         offset="100%"
                     ></stop>
                 </linearGradient>
             </svg>
         </div>
     );
+};
+
+Spinner.propTypes = {
+    height: PropTypes.string,
+};
+
+Spinner.defaultProps = {
+    height: "100vh",
 };
 
 export default Spinner;
