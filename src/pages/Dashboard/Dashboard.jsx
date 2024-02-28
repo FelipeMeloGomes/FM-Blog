b083ed04c42213c9e751144d4699917f5a20e599
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-28 02:46:03 -0300

    feat: add Spinner Dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,71 +10,84 @@ import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useDeleteDocument } from "../../hooks/useDeleteDocument";
 
 // components
-import TitleParagraph from "./../../components/TitleParagraph/TitleParagraph";
+import TitleParagraph from "../../components/TitleParagraph/TitleParagraph";
+import Spinner from "../../components/Spinner/Spinner";
 
-const Dashboard = () => {
+const Dashboard = ({ createdBy }) => {
     const { user } = useAuthValue();
     const uid = user.uid;
 
-    // posts do usuario
+    // posts users
     const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
-
+    // delete posts
     const { deleteDocument } = useDeleteDocument("posts");
 
     if (loading) {
         return;
     }
+
     return (
         <div className={styles.dashboard}>
             <TitleParagraph
                 title="Dashboard"
                 paragraph="Gerencie os seus posts"
             />
-            {posts && posts.length === 0 ? (
-                <div className={styles.noposts}>
-                    <p>Não foram encontrados posts</p>
-                    <Link to="/posts/create" className="btn">
-                        Criar primeiro post
-                    </Link>
-                </div>
+            {loading ? (
+                <Spinner />
             ) : (
                 <>
-                    <div className={styles.post_header}>
-                        <span>Título</span>
-                        <span>Ações</span>
-                    </div>
+                    {posts && posts.length === 0 ? (
+                        <div className={styles.noposts}>
+                            <p>Não foram encontrados posts</p>
+                            <Link to="/posts/create" className="btn">
+                                Criar primeiro post
+                            </Link>
+                        </div>
+                    ) : (
+                        <>
+                            <div className={styles.post_header}>
+                                <span>Título</span>
+                                <span>Ações</span>
+                            </div>
 
-                    {posts &&
-                        posts
-                            .sort((a, b) => a.title.localeCompare(b.title))
-                            .map((post) => (
-                                <div key={post.id} className={styles.post_row}>
-                                    <p>{post.title}</p>
-                                    <div>
-                                        <Link
-                                            to={`/posts/${post.id}`}
-                                            className="btn btn-outline"
+                            {posts &&
+                                posts
+                                    .sort((a, b) =>
+                                        a.title.localeCompare(b.title)
+                                    )
+                                    .map((post) => (
+                                        <div
+                                            key={post.id}
+                                            className={styles.post_row}
                                         >
-                                            Ver
-                                        </Link>
-                                        <Link
-                                            to={`/posts/edit/${post.id}`}
-                                            className="btn btn-outline"
-                                        >
-                                            Editar
-                                        </Link>
-                                        <button
-                                            onClick={() =>
-                                                deleteDocument(post.id)
-                                            }
-                                            className="btn btn-outline btn-danger"
-                                            alt="Excluir"
-                                        >
-                                            Excluir
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
+                                            <p>{post.title}</p>
+                                            <div>
+                                                <Link
+                                                    to={`/posts/${post.id}`}
+                                                    className="btn btn-outline"
+                                                >
+                                                    Ver
+                                                </Link>
+                                                <Link
+                                                    to={`/posts/edit/${post.id}`}
+                                                    className="btn btn-outline"
+                                                >
+                                                    Editar
+                                                </Link>
+                                                <button
+                                                    onClick={() =>
+                                                        deleteDocument(post.id)
+                                                    }
+                                                    className="btn btn-outline btn-danger"
+                                                    alt="Excluir"
+                                                >
+                                                    Excluir
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                        </>
+                    )}
                 </>
             )}
         </div>
