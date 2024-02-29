625fb47d8a22d866053602164d075010d489ddb6
Felipe Melo <felipemelogomes36@gmail.com>
2024-02-29 19:46:18 -0300

    fix: deleted commentaries

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,11 +1,20 @@
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from "firebase/auth";
+// Firebase
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    updateProfile,
+    signOut,
+} from "firebase/auth";
+
+// Hooks React
 import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
     const [state, setState] = useState({
         error: null,
         loading: false,
-        cancelled: false // Estado para lidar com vazamento de memória
+        cancelled: false,
     });
 
     const auth = getAuth();
@@ -13,7 +22,8 @@ export const useAuthentication = () => {
     const handleErrorMessage = (error) => {
         let systemErrorMessage;
         if (error.message.includes("Password")) {
-            systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+            systemErrorMessage =
+                "A senha precisa conter pelo menos 6 caracteres.";
         } else if (error.message.includes("email-already")) {
             systemErrorMessage = "E-mail já cadastrado.";
         } else if (error.message.includes("user-not-found")) {
@@ -36,7 +46,11 @@ export const useAuthentication = () => {
         checkIfIsCancelled();
         setState({ ...state, loading: true });
         try {
-            const { user } = await createUserWithEmailAndPassword(auth, data.email, data.password);
+            const { user } = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            );
             await updateProfile(user, { displayName: data.displayName });
             return user;
         } catch (error) {
@@ -63,7 +77,7 @@ export const useAuthentication = () => {
     };
 
     useEffect(() => {
-        return () => setState({ ...state, cancelled: true }); // Função de limpeza para evitar vazamento de memória
+        return () => setState({ ...state, cancelled: true });
     }, []);
 
     return {
