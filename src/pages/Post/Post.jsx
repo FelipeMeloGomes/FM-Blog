aea936e481c2b8bcc3962a40d499d9fff65d7921
Felipe Melo <felipemelogomes36@gmail.com>
2024-03-05 20:06:23 -0300

    feat: add adjust css Post

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,67 +2,76 @@
 import styles from "./Post.module.css";
 
 // React Router Dom
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 // Icons
-import { FiHash } from "react-icons/fi";
+import { MdArrowBack } from "react-icons/md";
 
 // Hooks
 import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 // Components
 import Spinner from "../../components/Spinner/Spinner";
+import LikeButton from "../../components/LikeButton/LikeButton";
 
 const Post = () => {
     const { id } = useParams();
     const { document: post, loading } = useFetchDocument("posts", id);
     return (
-        <div className={styles.post_container}>
-            {loading ? (
-                <Spinner />
-            ) : (
-                post && (
-                    <>
-                        <div className={styles.containerImg}>
-                            <figure>
-                                <img
-                                    src={post.image}
-                                    alt={post.title}
-                                    className={styles.card__image}
-                                    loading="lazy"
+        <>
+            <div className={styles.post_container}>
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    post && (
+                        <>
+                            <div className={styles.containerImg}>
+                                <figure>
+                                    <img
+                                        src={post.image}
+                                        alt={post.title}
+                                        className={styles.card__image}
+                                        loading="lazy"
+                                    />
+                                </figure>
+                                <p className={styles.createdby}>
+                                    Por: {post.createdBy}
+                                </p>
+                            </div>
+                            <div>
+                                <h1 className={styles.containerTitle}>
+                                    {post.title}
+                                </h1>
+                                <div
+                                    className={styles.bodyText}
+                                    dangerouslySetInnerHTML={{
+                                        __html: post.body,
+                                    }}
                                 />
-                            </figure>
-                            <p className={styles.createdby}>
-                                Por: {post.createdBy}
-                            </p>
-                        </div>
-                        <div>
-                            <h1 className={styles.containerTitle}>
-                                {post.title}
-                            </h1>
-                            <div
-                                className={styles.bodyText}
-                                dangerouslySetInnerHTML={{ __html: post.body }}
-                            />
-                        </div>
-                        <div className={styles.containerText}>
-                            <h3>Este post trata sobre:</h3>
-                        </div>
-                        <div className={styles.tags}>
-                            {post.tagsArray &&
-                                post.tagsArray.map((tag, index) => (
-                                    <p key={`${tag}_${index}`}>
-                                        <span>
-                                            <FiHash />
-                                        </span>
-                                        {tag}
-                                    </p>
-                                ))}
-                        </div>
-                    </>
-                )
-            )}
-        </div>
+                            </div>
+                            <div className={styles.containerText}>
+                                <h3 className={styles.titleh3}>
+                                    Este post trata sobre:
+                                </h3>
+                            </div>
+                            <div className={styles.tags}>
+                                {post.tagsArray &&
+                                    post.tagsArray.map((tag, index) => (
+                                        <p key={`${tag}_${index}`}>{tag}</p>
+                                    ))}
+                            </div>
+
+                            <div className={styles.btnArrow}>
+                                <Link to="/" className="btn btn-outline">
+                                    <MdArrowBack className="icon_font" />
+                                </Link>
+                                <LikeButton />
+                            </div>
+                        </>
+                    )
+                )}
+            </div>
+        </>
     );
 };
 
