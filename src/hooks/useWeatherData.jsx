2afe18b11ab10d8a71c4133d1d51f28644d9d6c8
Felipe Melo <felipemelogomes36@gmail.com>
2024-04-09 18:30:16 -0300

    refactor: refactoring custom hook useWeatherData

--- src/hooks/useWeatherData.jsx
+++ src/hooks/useWeatherData.jsx
@@ -1,8 +1,12 @@
 // Hooks
 import { useState } from "react";
 
+// Components
+import { Icon } from "../components/IconComponent";
+
 const useWeatherData = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [wicon, setWicon] = useState(<Icon name="cloud-sun" />);
     const [climaData, setClimaData] = useState({
         humidity: "",
         wind: "",
@@ -20,6 +24,35 @@ const useWeatherData = () => {
             const response = await fetch(url);
             const data = await response.json();
 
+            switch (data.weather[0].icon) {
+                case "01d":
+                case "01n":
+                    setWicon(<Icon name="sun" />);
+                    break;
+                case "02d":
+                case "02n":
+                    setWicon(<Icon name="cloud" />);
+                    break;
+                case "03d":
+                case "03n":
+                case "04d":
+                case "04n":
+                    setWicon(<Icon name="drizzle" />);
+                    break;
+                case "09d":
+                case "09n":
+                case "10d":
+                case "10n":
+                    setWicon(<Icon name="rain" />);
+                    break;
+                case "13d":
+                case "13n":
+                    setWicon(<Icon name="snow" />);
+                    break;
+                default:
+                    setWicon(<Icon name="sun" />);
+            }
+
             setClimaData({
                 humidity: data.main.humidity,
                 wind: data.wind.speed,
@@ -36,7 +69,7 @@ const useWeatherData = () => {
         }
     };
 
-    return { isLoading, climaData, fetchData };
+    return { isLoading, climaData, fetchData, wicon, Icon };
 };
 
 export default useWeatherData;
