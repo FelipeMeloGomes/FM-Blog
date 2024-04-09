54bbd2f07704a8f53567ea053f06d188cf4e64ad
Felipe Melo <felipemelogomes36@gmail.com>
2024-04-09 16:35:01 -0300

    feat: add new icons

--- src/components/IconComponent/Icon.jsx
+++ src/components/IconComponent/Icon.jsx
@@ -6,15 +6,33 @@ import {
     FaPlus,
     FaUser,
     FaLock,
+    FaCloudSun,
 } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import { FiLogOut, FiAtSign } from "react-icons/fi";
-import { MdOutlinePostAdd, MdDashboard } from "react-icons/md";
+import { MdOutlinePostAdd, MdDashboard, MdArrowBack } from "react-icons/md";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
+import { WiHumidity, WiWindy } from "react-icons/wi";
 
 const Icon = ({ name, ...props }) => {
     switch (name) {
         case "user":
             return <FaUser {...props} />;
+        case "cloud-sun":
+            return <FaCloudSun {...props} />;
+        case "humidity":
+            return <WiHumidity {...props} />;
+        case "windy":
+            return <WiWindy {...props} />;
+        case "arrow-back":
+            return <MdArrowBack {...props} />;
+        case "search":
+            return <CiSearch {...props} />;
+        case "github":
+            return <FaGithub {...props} />;
+        case "linkedin":
+            return <FaLinkedin {...props} />;
         case "sign":
             return <FiAtSign {...props} />;
         case "lock":
