15d77128ead2927268172d7eed3b075792524d85
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-03 17:25:29 -0300

    feat: add toolbar options Editor

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -6,6 +6,22 @@ import "react-quill/dist/quill.snow.css";
 import { forwardRef } from "react";
 
 export const Editor = forwardRef(({ value, onChange }, ref) => {
+    const toolbarOptions = [
+        [{ font: [] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ size: ["small", false, "large", "huge"] }],
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote", "code-block"],
+        ["link"],
+        [{ header: 1 }, { header: 2 }],
+        [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+        [{ script: "sub" }, { script: "super" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ direction: "rtl" }],
+        [{ color: [] }, { background: [] }],
+        [{ align: [] }],
+        ["clean"],
+    ];
     return (
         <ReactQuill
             className="editor"
@@ -14,6 +30,7 @@ export const Editor = forwardRef(({ value, onChange }, ref) => {
             theme="snow"
             placeholder="Digite o conteúdo aqui"
             ref={ref}
+            modules={{ toolbar: toolbarOptions }}
         />
     );
 });
