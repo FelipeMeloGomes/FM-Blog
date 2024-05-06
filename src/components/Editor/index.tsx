68818ecd01f849adecfd3f072023a4c37780b49e
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-06 16:18:20 -0300

    feat: add typescript component Editor

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,33 @@
+// React Quill
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+// React
+import { forwardRef } from "react";
+import { EditorRef, EditorProps } from "./types";
+
+export const Editor = forwardRef<EditorRef, EditorProps>(
+    ({ value, onChange }, ref) => {
+        const toolbarOptions = [
+            [{ font: [] }],
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            ["bold", "italic", "underline", "strike"],
+            ["blockquote", "code-block"],
+            ["link"],
+            ["clean"],
+        ];
+        return (
+            <ReactQuill
+                className="editor"
+                value={value}
+                onChange={onChange}
+                theme="snow"
+                placeholder="Digite o conteúdo aqui"
+                ref={ref}
+                modules={{ toolbar: toolbarOptions }}
+            />
+        );
+    }
+);
+
+export default Editor;
