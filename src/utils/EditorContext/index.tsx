fa748fcdf91a16ed5daf230963df358df65c02d0
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-06 16:21:34 -0300

    feat: add EditorContext typescript

--- src/utils/EditorContext/index.tsx
+++ src/utils/EditorContext/index.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 const EditorContext = () => {
     const [content, setContent] = useState("");
-    const handleEditorChange = (content:any) => {
+    const handleEditorChange = (content: any) => {
         setContent(content);
     };
     return { handleEditorChange, content, setContent };
 };
 
-export default EditorContext;
+export { EditorContext };
