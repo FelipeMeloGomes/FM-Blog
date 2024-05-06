f97d0ce0a34a50f46ea50b18621de5757d09252c
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-06 19:52:54 -0300

    feat: add typescript page notfound

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,40 @@
+// Estilos css
+import styles from "./NotFound.module.css";
+
+// Components
+import { TextField } from "../../components/TextField";
+
+// Img
+import NotFoundImg from "./assets/NotFoundImg.webp";
+
+// React Router Dom
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <div className={styles.container}>
+            <TextField
+                color="black"
+                title="404"
+                paragraph="Página Não Encontrada Desculpe."
+            />
+            <div className={styles.containerBtn}>
+                <Link to="/" className="btn btn-outline">
+                    Home
+                </Link>
+            </div>
+            <div className={styles.center}>
+                <figure>
+                    <img
+                        className={styles.containerImg}
+                        src={NotFoundImg}
+                        alt="not found"
+                        loading="lazy"
+                    />
+                </figure>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
