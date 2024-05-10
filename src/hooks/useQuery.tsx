60cc5fd25feea714e5e733096c76038261458f8c
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-10 15:56:12 -0300

    refactor: add typescript hook useQuery

---
+++ src/hooks/useQuery.tsx
@@ -0,0 +1,12 @@
+import { useMemo } from "react";
+import { useLocation, Location } from "react-router-dom";
+
+export const useQuery = (): URLSearchParams => {
+    const location: Location = useLocation();
+    const searchParams = useMemo(
+        () => new URLSearchParams(location.search),
+        [location.search]
+    );
+
+    return searchParams;
+};
