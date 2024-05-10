3db075a2e2f26baf038f2b56f7018eeec829c22c
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-10 17:18:48 -0300

    refactor: add typescript useDeletedDocument

---
+++ src/hooks/useDeleteDocument.tsx
@@ -0,0 +1,71 @@
+import { useState, useEffect, useReducer } from "react";
+import { db } from "../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
+
+interface DeleteState {
+    loading: boolean | null;
+    error: string | null;
+}
+
+type DeleteAction =
+    | { type: "LOADING" }
+    | { type: "DELETED_DOC" }
+    | { type: "ERROR"; payload: string };
+
+const initialState: DeleteState = {
+    loading: null,
+    error: null,
+};
+
+const deleteReducer = (
+    state: DeleteState,
+    action: DeleteAction
+): DeleteState => {
+    switch (action.type) {
+        case "LOADING":
+            return { loading: true, error: null };
+        case "DELETED_DOC":
+            return { loading: false, error: null };
+        case "ERROR":
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const useDeleteDocument = (docCollection: string) => {
+    const [{ loading, error }, dispatch] = useReducer(
+        deleteReducer,
+        initialState
+    );
+
+    const [isCancelled, setIsCancelled] = useState<boolean>(false);
+
+    const checkCancelBeforeDispatch = (action: DeleteAction) => {
+        if (!isCancelled) {
+            dispatch(action);
+        }
+    };
+
+    const deleteDocument = async (id: string) => {
+        checkCancelBeforeDispatch({ type: "LOADING" });
+
+        try {
+            await deleteDoc(doc(db, docCollection, id));
+            checkCancelBeforeDispatch({ type: "DELETED_DOC" });
+        } catch (error: any) {
+            const errorMessage =
+                "message" in error
+                    ? error.message
+                    : "Ocorreu um erro ao excluir o documento.";
+
+            checkCancelBeforeDispatch({ type: "ERROR", payload: errorMessage });
+        }
+    };
+
+    useEffect(() => {
+        return () => setIsCancelled(true);
+    }, []);
+
+    return { deleteDocument, loading, error };
+};
