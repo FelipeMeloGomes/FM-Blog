894f26159674e8e2780cd54121068449171f62ec
Felipe Melo <felipemelogomes36@gmail.com>
2024-09-18 05:03:45 -0300

    Remove `vite-plugin-imagemin` and clean up configuration
    - Uninstalled `vite-plugin-imagemin` package
    - Removed references to `vite-plugin-imagemin` from `vite.config.js`
    - Cleaned up project dependencies and cache
    - Verified that the build process completes without errors
    
    This change fixes the build issue related to `vite-plugin-imagemin`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  build: {
-    cssCodeSplit: false,
-    minify: true,
-    commonjsOptions: {
-      include: [/linked-dep/, /node_modules/],
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-  plugins: [react(), viteImagemin()],
+  plugins: [react()],
 });
