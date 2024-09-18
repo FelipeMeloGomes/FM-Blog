388b739801031eb5d5ff920cf73a9bfe570232db
Felipe Melo <felipemelogomes36@gmail.com>
2024-09-18 04:49:04 -0300

    refactor: deleted Toastify container

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ToastContainer } from "react-toastify";
 import { ChakraProvider } from "@chakra-ui/react";
-import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
-      <ToastContainer />
     </ChakraProvider>
   </React.StrictMode>,
 );
