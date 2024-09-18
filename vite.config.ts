5d1109d17545a24a453d7510a07ebb1edfb8cdc9
Felipe Melo <felipemelogomes36@gmail.com>
2024-09-18 05:00:12 -0300

    refactor: build

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteImagemin from "vite-plugin-imagemin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        cssCodeSplit: false,
-        minify: true,
-        commonjsOptions: {
-            include: [/linked-dep/, /node_modules/],
-        },
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.includes("node_modules")) {
-                        return "vendor";
-                    }
-                },
-            },
-        },
+  build: {
+    cssCodeSplit: false,
+    minify: true,
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/],
     },
-    plugins: [react(), viteImagemin()],
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+  plugins: [react(), viteImagemin()],
 });
