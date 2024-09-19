708aa6841ad77ae737f9bb4316a33e79e2c67420
Felipe Melo <felipemelogomes36@gmail.com>
2024-09-19 14:30:15 -0300

    "chore: optimize Vite config for performance with Chakra UI"

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
+  build: {
+    minify: "esbuild",
+    target: "esnext",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          chakra: [
+            "@chakra-ui/react",
+            "@emotion/react",
+            "@emotion/styled",
+            "framer-motion",
+          ],
+          vendor: ["some-heavy-library"],
+        },
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {},
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  optimizeDeps: {
+    include: [
+      "@chakra-ui/react",
+      "@emotion/react",
+      "@emotion/styled",
+      "framer-motion",
+    ],
+  },
 });
