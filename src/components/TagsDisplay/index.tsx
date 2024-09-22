1e1b50e35751b0d05ca9f9434c70836e139c26a8
Felipe Melo <felipemelogomes36@gmail.com>
2024-09-22 04:42:06 -0300

    Refactored TagsDisplay component to use gray badges with an "outline" variant for a cleaner and more modern appearance.

--- src/components/TagsDisplay/index.tsx
+++ src/components/TagsDisplay/index.tsx
@@ -1,20 +1,20 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Badge, Flex, Text } from "@chakra-ui/react";
 import { TagsDisplayProps } from "./types";
 
 const TagsDisplay = ({ tags }: TagsDisplayProps) => {
   return (
     <Flex width="100%" justifyContent="center" gap={2} flexWrap="wrap" p={1}>
       {tags?.map((tag, index) => (
-        <Box
+        <Badge
           key={`${tag}_${index}`}
-          bg="gray.200"
-          color="gray.700"
+          colorScheme="blue"
+          variant="solid"
           px={3}
           py={1}
           borderRadius="md"
         >
           <Text fontSize="sm">{tag}</Text>
-        </Box>
+        </Badge>
       ))}
     </Flex>
   );
