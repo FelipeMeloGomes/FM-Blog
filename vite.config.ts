03aaa61a59fd13581b7deadfe82675ff8526050c
Felipe Melo <felipemelogomes36@gmail.com>
2024-10-03 05:08:49 -0300

    fix: update Vite config to include base path and enable sourcemaps for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,6 @@ export default defineConfig({
     sourcemap: false,
     cssCodeSplit: true,
     chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
-        },
-      },
-    },
   },
   resolve: {
     alias: {
