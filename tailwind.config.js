6c2e2ad84c1903d15cd5a38917b6c0a128c5d2b5
Felipe Melo <felipemelogomes36@gmail.com>
2024-10-04 18:15:57 -0300

    chore(tailwind): remove unnecessary animations from configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,15 @@ export default {
   theme: {
     extend: {
       animation: {
-        rotation: "rotation 0.75s linear infinite",
         float: "float 1.8s infinite alternate",
-        floating: "floating 1.8s infinite alternate",
       },
       screens: {
         sm: "800px",
       },
       keyframes: {
-        rotation: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" },
-        },
         float: {
           "0%": { transform: "translateY(0)" },
           "100%": { transform: "translateY(15px)" },
-        },
-        floating: {
-          "0%": { transform: "translateY(0)" },
-          "100%": { transform: "translateY(20px)" },
         },
       },
     },
